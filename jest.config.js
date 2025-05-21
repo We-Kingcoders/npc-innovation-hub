b1c14bf9abde096ae50cb7cabccc04a63992fd6b
devlutiasc <appdevlutiasc@gmail.com>
2025-05-21 11:09:01 +0200

    Merge pull request #29 from We-Kingcoders/my-feature-branch
    changes to repo

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js', '<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+      tsconfig: '<rootDir>/tsconfig.jest.json',
+    }],
+  },
+  //no need for this globals if you're already using the modern transform API with inline ts-jest config:
+  // globals: {
+  //   'ts-jest': {
+  //     tsconfig: '<rootDir>/tsconfig.jest.json',
+  //     useESM: true
+  //   }
+  // },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.stories.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/main.tsx',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <main className="bg-red-500">
+      {" "}
+      {/* Changed from div to main or Added role attribute  called main*/}
+      <h1 className="text-white">Hello World</h1>
+    </main>
+  );
+}
