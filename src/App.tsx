b59e5594558e02c1247cb6b5807ea0316db34b35
peace-jpg <peacemuteteri3@gmil.com>
2025-05-23 07:51:55 +0200

    Login page and SignUp form

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import "./index.css";
-import HeroSection from "./pages/landing/HeroSection";
-import HubMembersSection from "./pages/landing/HubMembersSection";
-import Expertise from "./pages/landing/Expertise";
-import Skills from "./pages/landing/Skills";
-import Mission from "./pages/landing/Mission";
-import TeamCaptain from "./pages/landing/TeamCaptain";
-import Footer from "./components/button/Footer";
+import AllRoutes from "./Routes/AllRoutes";
 export default function App() {
   return (
     <>
-    < HeroSection />
-    < HubMembersSection />
-    < Expertise />
-    < Skills />
-    < Mission />
-    < TeamCaptain />
-    < Footer />
+      <AllRoutes />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,61 @@
+export default function Header() {
+  return (
+    <header className="bg-white">
+      <div className="container mx-auto flex items-center justify-between py-4 px-6">
+        {/* Logo */}
+        <div className="text-xl font-semibold text-gray-800">
+          NpcInnovationHub
+        </div>
+
+        {/* Navigation Links */}
+        <nav>
+          <ul className="flex space-x-8">
+            <li>
+              <a
+                href="#"
+                className="text-teal-500 border-b-2 border-teal-500 pb-1"
+                style={{ color: "#0175C3" }}
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-gray-600 hover:text-teal-500"
+                style={{ color: "#0175C3" }}
+              >
+                AboutHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-gray-600 hover:text-teal-500"
+                style={{ color: "#0175C3" }}
+              >
+                Members
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-gray-600 hover:text-teal-500"
+                style={{ color: "#0175C3" }}
+              >
+                Projects
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Sign In Button */}
+        <div>
+          <button className="bg-blue-900 text-white px-4 py-2 rounded-full hover:bg-blue-700">
+            Sign In
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
