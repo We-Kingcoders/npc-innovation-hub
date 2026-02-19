80b25e079b1937f4ec5c55a9a1d186d1ad9a15d9
Entue250 <edouardoniyomugabo@gmail.com>
2026-02-19 11:23:48 +0200

    Updated App comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-// import React from "react";
-// import AllRoutes from "./Routes/AllRoutes";
-
-// const App: React.FC = () => {
-//   return <AllRoutes />;
-// };
-
-// export default App;
-
 /**
  * Main App Component
  *
  * Updated to include AuthProvider for authentication management.
  * This wraps your entire application to provide auth context.
  */
-
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import AllRoutes from "./Routes/AllRoutes";
