30efb0c867127bf2b784492c7d6889b3a7a41f15
Entue250 <edouardoniyomugabo@gmail.com>
2026-09-04 19:04:47 +0200

    Register /Admin-alumni route

--- src/Routes/AllRoutes.tsx
+++ src/Routes/AllRoutes.tsx
@@ -54,6 +54,7 @@ import AdminResources from "../pages/Admin-pages/AdminResources";
 import HireUsRequests from "../pages/Admin-pages/HireUsRequests";
 import AddResource from "../pages/Admin-pages/AddResource";
 import MemberManagement from "../pages/Admin-pages/MemberManagement";
+import AlumniManagementPage from "../pages/Admin-pages/AlumniManagementPage";
 import BlogTables from "../pages/Admin-pages/BlogTables";
 import ProjectTables from "../pages/Admin-pages/ProjectTables";
 import OTPVerification from "../pages/Login/OTPVerification";
@@ -459,6 +460,14 @@ const AllRoutes: React.FC = () => {
         element={
           <ProtectedRoute requiredRole="Admin">
             <MemberManagement />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/Admin-alumni"
+        element={
+          <ProtectedRoute requiredRole="Admin">
+            <AlumniManagementPage />
           </ProtectedRoute>
         }
       />
